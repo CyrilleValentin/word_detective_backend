3bb4414a864aa7d75a3c669e23d610e4b4c19656
Cyrille <cyrillekyrillos6@gmail.com>
2023-11-01 12:28:53 +0000

    integration of ranking api

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -18,22 +18,24 @@ class UserController extends Controller
      * @param Request request
      *  @return User
      */
-   
-     public function createUser(Request $request)
-     {
-     try {
-            $validateUser=Validator::make($request->all(),
-            [
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|unique:users',
-                'password' => ['required', 'confirmed'],
-            ]);
+
+    public function createUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'name' => 'required|string|max:255',
+                    'email' => 'required|string|email|unique:users',
+                    'password' => ['required', 'confirmed'],
+                ]
+            );
             if ($validateUser->fails()) {
                 return response()->json([
-                    'statut'=>'false',
+                    'statut' => 'false',
                     'message' => 'validation error',
                     'errors' => $validateUser->errors()
-                    ], 401);
+                ], 401);
             }
             $user = User::create([
                 'name' => $request->name,
@@ -41,100 +43,114 @@ class UserController extends Controller
                 'password' => Hash::make($request->password),
             ]);
             return response()->json([
-                'statut'=>'true',
+                'statut' => 'true',
                 'message' => 'Utilisateur crée avec succès',
                 'token' => $user->createToken("API TOKEN")->plainTextToken
-                ], 200);
-         }catch(\Trowable $th){
+            ], 200);
+        } catch (Trowable $th) {
             return response()->json([
-                'statut'=>'false',
+                'statut' => 'false',
                 'message' => $th->getMessage(),
-                ], 500);
-         }
-     }  
+            ], 500);
+        }
+    }
 
     /**
      * login User
      * @param Request request
      *  @return User
      */
-   
-     public function loginUser(Request $request)
-{
-    try {
-        $validateUser = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
 
-        if ($validateUser->fails()) {
+    public function loginUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'statut' => 'false',
+                    'message' => 'Validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'statut' => 'false',
+                    'message' => 'Email ou mot de passe ne correspondent pas aux enregistrements',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            return response()->json([
+                'statut' => 'true',
+                'message' => 'Connecté avec succès',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+        } catch (\Throwable $th) {
             return response()->json([
                 'statut' => 'false',
-                'message' => 'Validation error',
-                'errors' => $validateUser->errors()
-            ], 401);
+                'message' => $th->getMessage(),
+            ], 500);
         }
+    }
 
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json([
-                'statut' => 'false',
-                'message' => 'Email ou mot de passe ne correspondent pas aux enregistrements',
-            ], 401);
-        }
-
-        $user = User::where('email', $request->email)->first();
+    public function logout(Request $request)
+    {
+        $accessToken = $request->bearerToken();
+        $token = PersonalAccessToken::findToken($accessToken);
+        $token->delete();
         return response()->json([
             'statut' => 'true',
-            'message' => 'Connecté avec succès',
-            'token' => $user->createToken("API TOKEN")->plainTextToken
-        ], 200);
-    } catch (\Throwable $th) {
+            'message' => 'Déconnecté avec succès'
+        ]);
+    }
+
+    public function profile(Request $request)
+    {
         return response()->json([
-            'statut' => 'false',
-            'message' => $th->getMessage(),
-        ], 500);
-    }
-}
-
-public function logout(Request $request){
-    $accessToken=$request->bearerToken();
-    $token=PersonalAccessToken::findToken($accessToken);   
-    $token->delete(); 
-    return response()->json([
-        'statut' => 'true',
-        'message' => 'Déconnecté avec succès']);  
-}
-
-public function profile(Request $request){
-    return response()->json([
-        'statut' => 'true',
-        'message' => 'Profil de l\'utilisateur',
-        'data'=> $request->user()
-    ]);  
-}
-
-public function updateScore(Request $request)
-{
-    // Récupérez l'utilisateur à partir du token (similaire à votre fonction de déconnexion)
-    $accessToken = $request->bearerToken();
-    $user = PersonalAccessToken::findToken($accessToken)->tokenable;
-
-    if (!$user) {
-        return response()->json(['message' => 'Token invalide'], 401);
+            'statut' => 'true',
+            'message' => 'Profil de l\'utilisateur',
+            'data' => $request->user()
+        ]);
     }
 
-    // Récupérez le nouveau score depuis la demande (par exemple, depuis un champ "score" dans la demande)
-    $newScore = $request->input('score');
+    public function updateScore(Request $request)
+    {
+        // Récupérez l'utilisateur à partir du token (similaire à votre fonction de déconnexion)
+        $accessToken = $request->bearerToken();
+        $user = PersonalAccessToken::findToken($accessToken)->tokenable;
 
-    // Mettez à jour le score de l'utilisateur
-    $user->score = $newScore;
-    $user->save();
+        if (!$user) {
+            return response()->json(['message' => 'Token invalide'], 401);
+        }
 
-    return response()->json([
-        'statut' => 'true',
-        'message' => 'Score mis à jour avec succès',
-        'user' => $user
-    ]);
-}
+        // Récupérez le nouveau score depuis la demande (par exemple, depuis un champ "score" dans la demande)
+        $newScore = $request->input('score');
+
+        // Mettez à jour le score de l'utilisateur
+        $user->score = $newScore;
+        $user->save();
+
+        return response()->json([
+            'statut' => 'true',
+            'message' => 'Score mis à jour avec succès',
+            'user' => $user
+        ]);
+    }
+
+    public function classement()
+    {
+        $users = User::orderBy('score', 'desc')->get();
+        return response()->json([
+            'statut' => 'true',
+            'message' => 'Classement des joueurs',
+            'users' => $users,
+        ]);
+    }
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/auth/register',[UserController::class ,'createUser']);
 Route::post('/auth/login',[UserController::class ,'loginUser']);
-
+Route::get('/auth/classement',[UserController::class ,'classement']);
 Route::group(['middleware'=>['auth:sanctum']],function () {
     Route::get('/auth/profile',[UserController::class ,'profile']);
     Route::delete('/auth/logout',[UserController::class ,'logout']);
     Route::post('/auth/update-score',[UserController::class ,'updateScore']);
+   
 });
